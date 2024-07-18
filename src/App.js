c843e29a7b3356670ed596db7b6cb67ff1b20668
shalmalee98 <shalmaleeshenolikar@gmail.com>
2024-07-18 17:21:44 -0400

    github pages 404 fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
